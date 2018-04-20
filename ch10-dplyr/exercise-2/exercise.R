1e17a965153b4355bcb41de9fe1b50914a922676
Mrinal Sharma <msharma2@cs.washington.edu>
2018-04-20 15:14:10 -0700

    Finished Exercise 2

--- ch10-dplyr/exercise-2/exercise.R
+++ ch10-dplyr/exercise-2/exercise.R
@@ -8,41 +8,48 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
-
+install.packages("dplyr")
+library("dplyr")
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+nrow(distinct(vehicles,make))
 
 # Filter the data set for vehicles manufactured in 1997
-
+cars_97 <- filter(vehicles, year==1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+cars_97 <- arrange(cars_97, hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+cars_97 <- mutate(cars_97, average = (hwy + cty) / 2)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+efficient <- filter(vehicles, drive == '2-Wheel Drive', cty > 20)
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+worst_car <- filter(efficient, hwy == min(hwy))
+worst_hwy <- select(worst_car, id)
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+filtering <- function(make_choice, year_choice) {
+  cut_down <- filter(vehicles, make == make_choice, year == year_choice)
+  filtered <- filter(filtered, hwy == max(hwy))
+  efficiency <- select(filtered, model)
+  efficiency
+}
 
 # What was the most efficient Honda model of 1995?
-
+filtering("Honda", 1995)
